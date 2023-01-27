d1b7cc55b8e5b999cdff6d0c9ef8ce8822f25992
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-01-27 10:49:41 +0000

    :fire: Logic of MapSubscription

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,6 @@ import {
 } from "apollo-server-core";
 import http from "http";
 import { connectDB } from "./utils/database.js";
-import { Disaster } from "./modules/Disaster/models/DisasterMongoose.js";
 //cors
 var cors = require("cors");
 
@@ -64,48 +63,3 @@ async function startApolloServer(typeDefs, resolvers) {
 }
 
 startApolloServer();
-
-// //sending emails to subscribers
-
-// //cron.schedule("0 */2 * * *", () => {
-// cron.schedule("*/10 * * * * *", function () {
-//   Disaster.find({})
-//     .populate("pins texts mapLayers")
-//     .exec((err, disasters) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         disasters.forEach((disaster) => {
-//           //if (disaster.subscriber.length !== 0) {
-//           let isUpdate = false;
-//           disaster.pins.forEach((pin) => {
-//             if (pin.date.getTime() > disaster.lastSentEmail.getTime()) {
-//               isUpdate = true;
-//             }
-//           });
-//           disaster.texts.forEach((text) => {
-//             if (text.date > disaster.lastSentEmail) {
-//               isUpdate = true;
-//             }
-//           });
-//           disaster.mapLayers.forEach((mapLayer) => {
-//             if (mapLayer.date > disaster.lastSentEmail) {
-//               isUpdate = true;
-//             }
-//           });
-//           if (isUpdate) {
-//             console.log("has been updated");
-
-//             Disaster.findOneAndUpdate(
-//               { _id: disaster._id },
-//               { lastSentEmail: Date.now() }
-//             );
-//             // send email to subscribers
-//             // update lastSentEmail to current time
-//           }
-
-//           //}
-//         });
-//       }
-//     });
-// });
