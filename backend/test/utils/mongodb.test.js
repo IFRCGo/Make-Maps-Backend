ffebfed0d6af1200dbad6457411fa01faf279a20
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-02-01 19:08:42 +0000

    :white_check_mark Add Test for Testing MongoDB

--- backend/test/utils/mongodb.test.js
+++ backend/test/utils/mongodb.test.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
-const uri = process.env.MONGO_URL;
+//const uri = process.env.MONGO_URL;
+const uri =
+  "mongodb+srv://piraveenankirupakaran:RHFixkKdutpicbaY@cluster0.3nqu1aq.mongodb.net/?retryWrites=true&w=majority";
 describe("MongoDB connection", () => {
   it("Connects to the MongoDB server", async () => {
     const client = await MongoClient.connect(uri, {
