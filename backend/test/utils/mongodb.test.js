d560e36da3c4a054a75d2011c63ee7db8e1b3558
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-02-01 19:05:35 +0000

    :rotating_light: Add Tests

---
+++ backend/test/utils/mongodb.test.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+const uri = process.env.MONGO_URL;
+describe("MongoDB connection", () => {
+  it("Connects to the MongoDB server", async () => {
+    const client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+    });
+    expect(client).toBeDefined();
+    await client.close();
+  });
+});
