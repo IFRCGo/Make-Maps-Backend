92759d5e9da88e198c5e486875c202ddc649f7bf
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-03-10 20:36:51 +0000

    Added Coverage files

--- backend/jest.config.mjs
+++ backend/jest.config.mjs
@@ -34,12 +34,7 @@ export default {
   // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ["html"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
